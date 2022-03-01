119f3d7ccea22232b9637835a75901011d051e49
vladvlas <vladvlas175@gmail.com>
2022-03-01 12:58:45 +0300

    before select

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import PostForm from "./components/PostForm";
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+
+import './styles/App.css'
 
 function App() {
+
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'JS', body: 'Description'},
+    {id: 2, title: 'C#', body: 'Description'},
+    {id: 3, title: 'Python', body: 'Description'},
+  ]);
+
+
+ const createPost = (newPost) => {
+  setPosts([...posts, newPost])
+ }
+
+ const removePost = (post) => {
+  setPosts(posts.filter(p => p.id !== post.id))
+ }
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PostForm create={createPost} />
+      <hr style={{margin: '15px'}}/>
+      <div>
+        <select>
+          <option>by name</option>
+          <option>by description</option>
+        </select>
+      </div>
+      {posts.length !== 0
+        ?
+        <PostList remove={removePost} posts={posts} title={'Todo list'}/>
+        :
+        <h1 style={{textAlign: 'center'}}>No todos</h1>
+      }
     </div>
-  );
+  )
+
 }
+
 
 export default App;
---
+++ src/components/PostForm.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import MyButton from './UI/button/MyButton'
+import MyInput from './UI/input/MyInput'
+
+const PostForm = ({create}) => {
+
+    const [post, setPost] = useState({title: '', body: ''});
+
+    const addNewPost = (e) => {
+        e.preventDefault();
+        const newPost = {
+            ...post, id: Date.now()
+        }
+        create(newPost)
+        setPost({title: '', body: ''})
+      }
+
+  return (
+    <form>
+        <MyInput
+          value={post.title}
+          onChange={e => setPost({...post, title: e.target.value})}
+          type='text'
+          placeholder='Post name'
+        />
+        <MyInput 
+          value={post.body}
+          onChange={e => setPost({...post, body: e.target.value})}
+          type='text' 
+          placeholder='Post description' />
+        <MyButton onClick={addNewPost}>Create post</MyButton>
+      </form>
+  )
+}
+
+export default PostForm
