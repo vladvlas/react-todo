986d1e3ea15f4f1af34382e70cba7a188176d0de
vladvlas <vladvlas175@gmail.com>
2022-03-02 12:36:53 +0300

    after loader

--- src/App.js
+++ src/App.js
@@ -1,42 +1,43 @@
-import React, { useMemo, useState } from "react";
+import axios from "axios";
+import React, { useEffect, useMemo, useState } from "react";
+import PostService from "./API/PostServise";
 import PostFilter from "./components/PostFilter";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
+import MyLoader from "./components/UI/loader/MyLoader";
 import MyModal from "./components/UI/modal/MyModal";
 import MySelect from "./components/UI/select/MySelect";
+import { usePosts } from "./hooks/usePosts";
 
 import './styles/App.css'
 
 function App() {
 
-  const [posts, setPosts] = useState([
-    {id: 1, title: 'JS', body: 'Description'},
-    {id: 2, title: 'C#', body: '2Description'},
-    {id: 3, title: 'Python', body: '3Description'},
-  ]);
-
+  const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({sort: '', query: ''});
   const [modal, setModal] = useState(false);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [isPostsLoading, setIsPostsLoading] = useState(false)
 
+  
 
-  const sortedPosts = useMemo(() => {
-    console.log('12121212')
-    if(filter.sort) {
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-    }
-    return posts
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-  }, [filter.query, sortedPosts])
+  useEffect(() => {
+    fetchPosts()
+  }, [])
 
 
  const createPost = (newPost) => {
   setPosts([...posts, newPost]);
   setModal(false)
+ }
+
+ async function fetchPosts() {
+   setIsPostsLoading(true);
+   const posts = await PostService.getAll();
+   setPosts(posts);
+   setIsPostsLoading(false);
  }
 
  const removePost = (post) => {
@@ -62,7 +63,13 @@ function App() {
       
       <hr style={{margin: '15px'}}/>
       <PostFilter filter={filter} setFilter={setFilter} />
-      <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Todo list'}/>
+      {isPostsLoading
+        ? <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: '50px'}}>
+            <MyLoader />
+        </div>
+        : <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Todo list'}/>
+      }
+      
     </div>
   )
 
